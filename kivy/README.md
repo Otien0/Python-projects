0b6b3812504a2a3549a8eb0c0af20f2413296fbf
mourice-oduor <otienomaurice364@gmail.com>
2020-04-22 16:19:51 -0400

    Update README.md

---
+++ kivy/README.md
@@ -0,0 +1,5 @@
+#created same hotel booking form 
+using kivy
+
+#The simpler one compared to tkinter 
+few lines of code
---
+++ kivy/back.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("hotel.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTs hotel (id INTEGER PRIMARY KEY , name TEXT , address TEXT , phone_number INTEGER, no_of_days INTEGER , room_type TEXT , total INTEGER)")
+    conn.commit()
+    conn.close()
+    
+def insert(name,address,phone_number,no_of_days,room_type,total):
+    from back import calculation
+    conn=sqlite3.connect("hotel.db")
+    cur=conn.cursor()
+    val =calculation(no_of_days,room_type)
+    cur.execute("INSERT INTO hotel (name,address,phone_number,no_of_days,room_type,total) VALUES(?,?,?,?,?,?)",(name,address,phone_number,no_of_days,room_type,val))
+    conn.commit()
+    conn.close()
+    view()
+
+def view():
+    conn=sqlite3.connect("hotel.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM hotel")
+    row=cur.fetchall()
+    conn.close()
+    return row
+
+def search(name="",address="",phone_number="",room_type="",no_of_days="",total=""):
+    conn=sqlite3.connect("hotel.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM hotel WHERE name=? OR address=? OR phone_number=? OR room_type=? OR no_of_days=? OR total=?",(name,address,phone_number,no_of_days,room_type,total,))
+    row=cur.fetchall()
+    conn.close()
+    return row
+
+def delete(id):
+    conn=sqlite3.connect("hotel.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM hotel where id=?",(id))
+    conn.commit()
+    conn.close()
+
+def update(id,name,phone_number,room_type,no_of_days,total):
+    from back import calculation
+    conn=sqlite3.connect("hotel.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE hotel SET name=? or address=? or phone_number=? or room_type=? or no_of_days? or total=? where id=?",(name,address,phone_number,no_of_days,room_type,calculation(no_of_days,room_type),id))
+    conn.commit()
+    conn.close()
+    
+    
+def calculation(no_of_days,room_type):
+    if room_type==("normal" or "NORMAL"):
+        total=int(no_of_days)*1500
+        return total
+    elif room_type==("KING" or "king"):
+        total=int(no_of_days)*1800
+        return total
+    elif room_type==("delux" or "DELUX"):
+        total=int(no_of_days)*2000
+        return total
+connect()
